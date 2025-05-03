e236c285d9eeffc44b0ff56c5a16f4248d70985b
Eder <ederbond@hotmail.com>
2025-05-03 08:34:47 -0300

    Adding a helper method to register WPF services.

---
+++ Naveasy.WPF/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Naveasy.WPF;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddNaveasy(this IServiceCollection self)
+    {
+        return self.AddSingleton<INavigationService, NavigationService>()
+            .AddSingleton<IWindowFactory, WindowFactory>();
+    }
+
+    public static IServiceCollection AddTransientForNavigation<TView, TViewModel>(this IServiceCollection self) where TView : Window
+    {
+        var windowType = typeof(TView);
+        var viewModelType = typeof(TViewModel);
+
+        if (WindowTypeRegistry.WindowModelToWindowDictionary.ContainsKey(viewModelType))
+        {
+            throw new ArgumentException($"The ViewModel {viewModelType.FullName} is already registered and cannot be registered again.");
+        }
+
+        WindowTypeRegistry.WindowModelToWindowDictionary.Add(viewModelType, windowType);
+
+        if (self.All(x => x.ServiceType != windowType))
+        {
+            self.AddTransient(windowType);
+        }
+
+        return self.AddTransient(viewModelType);
+    }
+}
--- Naveasy.WPF/WindowTypeRegistry.cs
+++ Naveasy.WPF/WindowTypeRegistry.cs
@@ -1,31 +1,8 @@
-﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Naveasy.WPF;
+﻿namespace Naveasy.WPF;
 
 public static class WindowTypeRegistry
 {
-    private static readonly Dictionary<Type, Type> WindowModelToWindowDictionary = [];
-
-    public static IServiceCollection AddTransientForNavigation<TView, TViewModel>(this IServiceCollection self) where TView: Window
-    {
-        var windowType = typeof(TView);
-        var viewModelType = typeof(TViewModel);
-
-        if (WindowModelToWindowDictionary.ContainsKey(viewModelType))
-        {
-            throw new ArgumentException($"The ViewModel {viewModelType.FullName} is already registered and cannot be registered again.");
-        }
-
-        WindowModelToWindowDictionary.Add(viewModelType, windowType);
-
-        if (self.All(x => x.ServiceType != windowType))
-        {
-            self.AddTransient(windowType);
-        }
-
-        return self.AddTransient(viewModelType);
-    }
+    public static Dictionary<Type, Type> WindowModelToWindowDictionary { get; } = [];
 
     public static Type ResolveWindowType(Type viewModelType)
     {
